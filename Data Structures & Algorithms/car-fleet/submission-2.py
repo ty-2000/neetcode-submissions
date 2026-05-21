6761103b2ea3437514e5db885370b60f108f70d7
Takeaki YAMAMOTO <102945453+ty-2000@users.noreply.github.com>
2026-05-20 21:03:55 -0400

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        # Sort position and speed concatenately based on the position in reverse
+        # After the sort, the car with a larger index cannot bypass the former one
+        # The time to the target for each cars can be calculated by (target - position[i]) / speed[i] (= time[i])
+        # With i < j, if time[i] < time[j]: these two are the fleet.
+
+        position, speed = zip(*sorted(zip(position, speed), reverse=True))
+        times = [(target - position[i]) / speed[i] for i in range(len(position))]
+        print(position)
+        print(speed)
+        print(times)
+        fleet = 1
+        for i in range(1, len(position)):
+            if times[i - 1] >= times[i]:
+                # car[i - 1] and car[i] are at the same fleet, arriving with the time[i - 1]
+                times[i] = times[i - 1]
+            else:
+                fleet += 1
+        return fleet
+        
